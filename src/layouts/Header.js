bdc53404c4e0e4bda71c9020c413b8d080832b23
Maciej Wszolek <maciej.wszolek98@gmail.com>
2021-06-23 20:26:38 +0200

    first

---
+++ src/layouts/Header.js
@@ -0,0 +1,16 @@
+import React from "react"
+import "../styles/Header.css"
+
+import img1 from "../images/header1.jpg"
+import img2 from "../images/header2.jpg"
+import img3 from "../images/header3.jpg"
+
+const Header = () => {
+  return (
+    <div>
+      <img src={img1} alt="city"></img>{" "}
+    </div>
+  )
+}
+
+export default Header
